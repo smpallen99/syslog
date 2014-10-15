33fdfba4d9375550920493de7db3d6c605e8f490
Steve Pallen <smpallen99@yahoo.com>
2014-10-15 12:34:57 -0400

    Upissue to version 0.5.0
    Version update for changes to support Elixir 1.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Syslog.Mixfile do
 
   def project do
     [app: :syslog,
-     version: "0.0.1",
+     version: "0.5.0",
      elixir: "~> 1.0.0",
      deps: deps]
   end
@@ -15,14 +15,6 @@ defmodule Syslog.Mixfile do
     [applications: []]
   end
 
-  # Dependencies can be hex.pm packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1"}
-  #
   # Type `mix help deps` for more examples and options
   defp deps do
     []
